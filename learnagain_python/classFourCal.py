513cff6cc48f9f0cde2415a6ff48c714c01c2099
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-01-05 00:28:26 +0900

    learnagain_python class concept tutorial

---
+++ learnagain_python/classFourCal.py
@@ -0,0 +1,13 @@
+class FourCal:
+    def setdata(self, first, second):
+        self.first=first
+        self.second=second
+    
+    def sum(self):
+        result = self.first+self.second
+        return result
+
+    def mul(self):
+        result = self.first*self.second
+        return result
+        
