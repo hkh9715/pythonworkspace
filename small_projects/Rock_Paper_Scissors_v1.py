5ec9c7e5f03e6183ae14df7c836c5a077d3ff103
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-02-23 23:44:44 +0900

    enter when new game starts

--- small_projects/Rock_Paper_Scissors_v1.py
+++ small_projects/Rock_Paper_Scissors_v1.py
@@ -3,39 +3,42 @@
 #update1 : make draw part / make format(make simply)
 #update2 : make 'play again' part
 #update3 : UpperLower / random
+#update4 : enter when new game starts
 
 
 while True:
     import random
-
-    a = input('Show your hand(rock, paper, scissors): ')
-    player = a.lower()
-    Type = ['rock', 'paper', 'scissors']
-    computer = random.choice(Type)
-    # print(computer)
+    try:
+        a = input('\nShow your hand(rock, paper, scissors): ')
+        player = a.lower()
+        Type = ['rock', 'paper', 'scissors']
+        computer = random.choice(Type)
+        # print(computer)
     
-    if player == computer:
-        print('Draw')
-    elif player == 'paper':
-        if computer == 'rock':
-            print('You win:', 'player {0}, computer {1}'.format(player, computer))
-        elif computer == 'scissors':
-            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+        if player == computer:
+            print('Draw')
+        elif player == 'paper':
+            if computer == 'rock':
+                print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            elif computer == 'scissors':
+                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
 
-    elif player == 'rock':
-        if computer == 'scissors':
-            print('You win:', 'player {0}, computer {1}'.format(player, computer))
-        else:
-            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+        elif player == 'rock':
+            if computer == 'scissors':
+                print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            else:
+                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
 
-    elif player == 'scissors':
-        if computer == 'paper':
-            print('You win:', 'player {0}, computer {1}'.format(player, computer))
-        else:
-            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
-    
+        elif player == 'scissors':
+            if computer == 'paper':
+                print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            else:
+                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+    except:
+        print('please type one of these(rock, paper, scissors)')
+
     b = input('Want to play again? (y/n): ')
     command = b.lower()
     if command != 'y':
-        print('See you')
+        print('\nSee you')
         break
