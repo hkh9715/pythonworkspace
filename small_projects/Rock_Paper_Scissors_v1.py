ba4d8cbc76346d3e3ab4da31bfa10289ebb7d01c
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-02-24 18:37:26 +0900

    make error message by 'try&except raise'

--- small_projects/Rock_Paper_Scissors_v1.py
+++ small_projects/Rock_Paper_Scissors_v1.py
@@ -4,41 +4,59 @@
 #update2 : make 'play again' part
 #update3 : UpperLower / random
 #update4 : enter when new game starts
+#update5 : make error message by 'try&except'
 
+import random
+
+Type = ['rock', 'paper', 'scissors']
+def error1(e1):
+    if e1 not in Type:
+        raise ValueError
+# def error2(e2):
+#     if e2 != 'y':
+#         raise ValueError
 
 while True:
-    import random
+    a = input('\nShow your hand(rock, paper, scissors): ')
+    player = a.lower()
     try:
-        a = input('\nShow your hand(rock, paper, scissors): ')
-        player = a.lower()
-        Type = ['rock', 'paper', 'scissors']
-        computer = random.choice(Type)
-        # print(computer)
-    
-        if player == computer:
-            print('Draw')
-        elif player == 'paper':
-            if computer == 'rock':
-                print('You win:', 'player {0}, computer {1}'.format(player, computer))
-            elif computer == 'scissors':
-                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
-
-        elif player == 'rock':
-            if computer == 'scissors':
-                print('You win:', 'player {0}, computer {1}'.format(player, computer))
-            else:
-                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
-
-        elif player == 'scissors':
-            if computer == 'paper':
-                print('You win:', 'player {0}, computer {1}'.format(player, computer))
-            else:
-                print('You lose:', 'player {0}, computer {1}'.format(player, computer))
-    except:
+        error1(player)
+    except ValueError:
         print('please type one of these(rock, paper, scissors)')
+    computer = random.choice(Type)
+    # print(computer)
 
-    b = input('Want to play again? (y/n): ')
-    command = b.lower()
-    if command != 'y':
+    if player == computer:
+        print('Draw')
+    elif player == 'paper':
+        if computer == 'rock':
+            print('You win:', 'player {0}, computer {1}'.format(player, computer))
+        else:
+            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+
+    elif player == 'rock':
+        if computer == 'scissors':
+            print('You win:', 'player {0}, computer {1}'.format(player, computer))
+        else:
+            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+
+    elif player == 'scissors':
+        if computer == 'paper':
+            print('You win:', 'player {0}, computer {1}'.format(player, computer))
+        else:
+            print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+
+    # num_win = 
+    # b = input('Want to play again? (y/n): \nYour odds: {0}:{1}:{2}'.format(num_win, num_draw, num_lose))
+    b = input('Want to play again? Press ENTER: ')
+    # print(b)
+    if b is not None:
         print('\nSee you')
         break
+    # try:
+    #     error2(command)
+    # except ValueError:
+    #     print('If you want to, type y!')
+    # if command != 'y':
+    #     print('\nSee you')
+    #     break
