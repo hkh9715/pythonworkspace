4e439c7614ec1f85dd1e3b7c3bb0d6a7cd102f09
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-02-24 23:59:47 +0900

    make odds

--- small_projects/Rock_Paper_Scissors_v1.py
+++ small_projects/Rock_Paper_Scissors_v1.py
@@ -4,7 +4,7 @@
 #update2 : make 'play again' part
 #update3 : UpperLower / random
 #update4 : enter when new game starts
-#update5 : make error message by 'try&except'
+#update5 : make error message by 'try&except' / make sure to restart the game by 'continue' / make odds
 
 import random
 
@@ -15,6 +15,9 @@ def error1(e1):
 # def error2(e2):
 #     if e2 != 'y':
 #         raise ValueError
+num_win = 0
+num_draw = 0
+num_lose = 0
 
 while True:
     a = input('\nShow your hand(rock, paper, scissors): ')
@@ -22,41 +25,42 @@ while True:
     try:
         error1(player)
     except ValueError:
-        print('please type one of these(rock, paper, scissors)')
+        print('PLEASE type one of these(rock, paper, scissors)')
+        continue
     computer = random.choice(Type)
     # print(computer)
 
     if player == computer:
         print('Draw')
+        num_draw += 1
     elif player == 'paper':
         if computer == 'rock':
             print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            num_win += 1
         else:
             print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+            num_lose += 1
 
     elif player == 'rock':
         if computer == 'scissors':
             print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            num_win += 1
         else:
             print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+            num_lose += 1
 
     elif player == 'scissors':
         if computer == 'paper':
             print('You win:', 'player {0}, computer {1}'.format(player, computer))
+            num_win += 1
         else:
             print('You lose:', 'player {0}, computer {1}'.format(player, computer))
+            num_lose += 1
 
-    # num_win = 
-    # b = input('Want to play again? (y/n): \nYour odds: {0}:{1}:{2}'.format(num_win, num_draw, num_lose))
-    b = input('Want to play again? Press ENTER: ')
+    b = input('Your odds(win:draw:lose): {0}:{1}:{2} \nWant to play again? Press ENTER: '.format(num_win, num_draw, num_lose))
     # print(b)
-    if b is not None:
+    if b != "":
         print('\nSee you')
         break
-    # try:
-    #     error2(command)
-    # except ValueError:
-    #     print('If you want to, type y!')
-    # if command != 'y':
-    #     print('\nSee you')
-    #     break
+    else:
+        print('play again')
