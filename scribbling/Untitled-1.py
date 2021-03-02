1703ee405596c5199cdf4a020e260825a759a057
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-03-02 23:12:42 +0900

    scribbling

---
+++ scribbling/Untitled-1.py
@@ -0,0 +1,21 @@
+letter = ['a', 'b', 'c', 'd']
+
+
+# if 'a' in letter:
+#     print('yes')
+# else:
+#     print('no')
+
+# for i in range(0, 100, 2):
+#     print(i)
+
+
+# for i in enumerate(letter):
+#     print(i)
+
+# for i, v in enumerate(letter):
+#     print("index:{}, letter: {}".format(i,v))
+
+# ---------------------------------------------------------------------------
+import os
+print(os.name)
