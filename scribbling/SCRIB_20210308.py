42ec4e9622337e7bd3b434f8630421294e061ead
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-03-09 17:53:39 +0900

    learn formatting

---
+++ scribbling/SCRIB_20210308.py
@@ -0,0 +1,15 @@
+#Formatting
+
+#Using f
+# greek_gods = ['Zeus', 'Hera', 'Poseidon', 'Apollo', 'Bob']
+
+# for index, name in enumerate(greek_gods):
+#     print(f'at index {index} , we have : {name}')
+
+
+#Using format
+# sentence = "I ate {0} apples. so I was sick for {day} days.".format(10, day=3)
+# print(sentence)
+
+# import this
+import antigravity
